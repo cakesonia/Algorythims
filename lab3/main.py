08ef4ebb03e23c36afc7776a1de79e4be4320fe4
cakesonia <32911972+cakesonia@users.noreply.github.com>
2018-11-15 08:57:05 +0200

    code lab3

---
+++ lab3/main.py
@@ -0,0 +1,72 @@
+vertexes = set()
+
+
+def read_from_file():
+    with open('tribes.txt') as f:
+        data = f.readlines()
+    graph = {}
+
+    length = data.__len__()
+    for i in range(1, length):
+        nums = [int(n) for n in data[i].split()]
+        graph = add_value(graph, nums[0], nums[1])
+        graph = add_value(graph, nums[1], nums[0])
+        vertexes.add(nums[0])
+        vertexes.add(nums[1])
+
+    return graph, length
+
+
+def add_value(graph, x, y):
+    try:
+        z = graph[x]
+    except KeyError:
+        graph[x] = set()
+    graph[x].add(y)
+    return graph
+
+
+def odd(vertex, parity):
+    if vertex % 2 == 1:
+        parity[0] += 1
+    else:
+        parity[1] += 1
+    return parity
+
+
+def dfs(graph, start_vertex, parity):
+    parity = odd(start_vertex, parity)
+    try:
+        vertexes.remove(start_vertex)
+    except:
+        pass
+    for vertex in graph[start_vertex]:
+        if vertex in vertexes:
+            dfs(graph, vertex, parity)
+
+
+def dfs_main(graph):
+    tribes = []
+    while vertexes:
+        parity = [0, 0]
+        dfs(graph, int(vertexes.pop()), parity)
+        tribes.append(parity)
+    return tribes
+
+
+def count_pairs(tribes):
+    count = 0
+    length = tribes.__len__()
+    for i in range(0, length):
+        for j in range(i, length):
+            if i != j:
+                x = tribes[i][0] * tribes[j][1] + tribes[i][1] * tribes[j][0]
+                count += x
+    return count
+
+
+if __name__ == '__main__':
+    graph, length = read_from_file()
+    tribes = dfs_main(graph)
+    count = count_pairs(tribes)
+    print(count)
