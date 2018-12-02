c438cb94f6e52f45d450b23ecd21297e1f4d2649
cakesonia <32911972+cakesonia@users.noreply.github.com>
2018-12-02 19:24:48 +0200

    changes after review

--- lab3/main.py
+++ lab3/main.py
@@ -6,7 +6,7 @@ def read_from_file():
         data = f.readlines()
     graph = {}
 
-    length = data.__len__()
+    length = len(data)
     for i in range(1, length):
         nums = [int(n) for n in data[i].split()]
         graph = add_value(graph, nums[0], nums[1])
@@ -19,7 +19,7 @@ def read_from_file():
 
 def add_value(graph, x, y):
     try:
-        z = graph[x]
+        graph[x]
     except KeyError:
         graph[x] = set()
     graph[x].add(y)
@@ -34,33 +34,32 @@ def odd(vertex, parity):
     return parity
 
 
-def dfs(graph, start_vertex, parity):
-    parity = odd(start_vertex, parity)
-    try:
-        vertexes.remove(start_vertex)
-    except:
-        pass
-    for vertex in graph[start_vertex]:
-        if vertex in vertexes:
-            dfs(graph, vertex, parity)
+def dfs_main(given_graph):
+    tribes_list = []
 
-
-def dfs_main(graph):
-    tribes = []
+    def dfs(tribes, start_vertex, parity):
+        parity = odd(start_vertex, parity)
+        try:
+            vertexes.remove(start_vertex)
+        except:
+            pass
+        for vertex in tribes[start_vertex]:
+            if vertex in vertexes:
+                dfs(tribes, vertex, parity)
     while vertexes:
         parity = [0, 0]
-        dfs(graph, int(vertexes.pop()), parity)
-        tribes.append(parity)
-    return tribes
+        dfs(given_graph, int(vertexes.pop()), parity)
+        tribes_list.append(parity)
+    return tribes_list
 
 
-def count_pairs(tribes):
+def count_pairs(different_tribes):
     count = 0
-    length = tribes.__len__()
-    for i in range(0, length):
-        for j in range(i, length):
+    tribe_list = len(different_tribes)
+    for i in range(0, tribe_list):
+        for j in range(i, tribe_list):
             if i != j:
-                x = tribes[i][0] * tribes[j][1] + tribes[i][1] * tribes[j][0]
+                x = different_tribes[i][0] * different_tribes[j][1] + different_tribes[i][1] * different_tribes[j][0]
                 count += x
     return count
 
